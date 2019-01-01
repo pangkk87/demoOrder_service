6df0905263f0174e6831ae2493b1e3b6e7d7742b
plk <233332336@qq.com>
2019-01-02 00:20:15 +0800

    loadBalancer 来获取服务  客户端做负载均衡

--- src/main/java/cn/blackboss/order_service/OrderServiceApplication.java
+++ src/main/java/cn/blackboss/order_service/OrderServiceApplication.java
@@ -6,6 +6,13 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * 订单服务
+ * 引入 web -- web
+ *     cloud discovery  -- euraka discovery
+ *     cloud routing --  ribbon
+ *
+ */
 @SpringBootApplication
 public class OrderServiceApplication {
 
@@ -14,6 +21,12 @@ public class OrderServiceApplication {
 	}
 
 
+	/**
+	 * 包装设计模式
+	 * 包装了 httpClient
+	 *
+	 * LoadBalanced标识是一个负载均衡器
+	 */
 	@Bean
 	@LoadBalanced
 	public RestTemplate restTemplate(){
--- src/main/java/cn/blackboss/order_service/service/impl/ProductOrderServiceImpl.java
+++ src/main/java/cn/blackboss/order_service/service/impl/ProductOrderServiceImpl.java
@@ -3,21 +3,38 @@ package cn.blackboss.order_service.service.impl;
 import cn.blackboss.order_service.domain.ProductOrder;
 import cn.blackboss.order_service.service.ProductOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class ProductOrderServiceImpl implements ProductOrderService {
 
+//    @Autowired
+//    private RestTemplate restTemplate;
+
     @Autowired
-    private RestTemplate restTemplate;
+    private LoadBalancerClient loadBalancer;
+
     @Override
     public ProductOrder save(int userId, int productId) {
-
-        Object obj = restTemplate.getForObject("http://product-service/api/v1/product/find?id="+productId,Object.class);
+/**
+ *
+ * LoadBalancer的choose方法来
+ *   根据sid(service.id)获取服务列表
+ *   通过策略来过滤出一个服务地址（ip+端口） 策略分为 轮询 权重 随机
+ *   在通过restTemplate来调用服务   底层也是HTTPCLIENT.
+ */
+        ServiceInstance instance = loadBalancer.choose("product-service");
+        String url = String.format("http://%s%s/api/v1/product/find?id="+productId,instance.getHost(),instance.getPort());
+        RestTemplate restTemplate = new RestTemplate();
+        Object obj = restTemplate.getForObject(url,Map.class);
         System.out.println(obj);
 
         ProductOrder productOrder = new ProductOrder();
